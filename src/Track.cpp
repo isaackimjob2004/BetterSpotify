35d5887e22fa3ebe6a7ad0ec5e1cf206eaa3de9c
Isaac Kim <ik367@cornell.edu>
2026-08-25 18:52:35 -0700

    refactor(track src): implement constructors and insert operator

--- src/Track.cpp
+++ src/Track.cpp
@@ -1,5 +1,25 @@
 #include "Track.hpp"
+#include <iostream>
 #include <utility>
 
+Track::Track(std::string isrc, std::string title, std::vector<std::string> artists)
+    : isrc_(std::move(isrc)), 
+      title_(std::move(title)), 
+      artists_(std::move(artists)) {}
+
 Track::Track(std::string isrc, std::string title, std::string artist)
-    : isrc(std::move(isrc)), title(std::move(title)), artist(std::move(artist)) {}
+    : isrc_(std::move(isrc)),
+      title_(std::move(title)),
+      artists_{std::move(artist)} {}
+
+std::ostream& operator<<(std::ostream& os, const Track& track) {
+    os << "[TRACK " << track.isrc_ << "] \"" << track.title_ << "\" by ";
+    if (track.artists_.empty()) {
+        os << "Unknown Artist";
+    } else {
+        for (size_t i = 0; i < track.artists_.size(); ++i) {
+            os << track.artists_[i] << (i+1 < track.artists_.size() ? ", " : "");
+        }
+    }
+    return os;
+}
